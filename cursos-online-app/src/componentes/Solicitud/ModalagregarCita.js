e068eda5cca77345b78bc995d8cbc75c8224b0b2
Fatis5 <fheredia1121@conalepmex.edu.mx>
2022-10-18 01:05:38 -0500

    todo ok ya pasan id

--- cursos-online-app/src/componentes/Solicitud/ModalagregarCita.js
+++ cursos-online-app/src/componentes/Solicitud/ModalagregarCita.js
@@ -1,97 +1,236 @@
 import React from "react";
 import { Modal, Box, TextField, Grid } from "@material-ui/core";
+import { useState, useEffect } from "react";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import DateFnsUtils from "@date-io/date-fns";
+ import { alpha } from '@material-ui/core/styles'
+import {
+  catalogoEstatus,
+  catalogosComunes,
+} from "../../actions/CatalogosAction";
+import { obtenerUsuariosXRoles } from "../../actions/UsuarioAction";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import id from "date-fns/esm/locale/id/index.js";
+import { set } from "date-fns/esm";
 
-const ModalagregarCita = ({ openCita, handleOpenCita, setopenCita }) => {
+const ModalagregarCita = ({
+  openCita,
+  handleOpenCita,
+  setopenCita,
+  idSolicitud,
+}) => {
+  const [infoCita, setinfoCita] = useState({
+    fechaInicio: new Date(),
+    fechaFin: new Date(),
+    horaInicio: new Date(),
+    idPrimeroEn: "",
+    idVisitador: "",
+    observaciones: "",
+    idEstatus: "",
+    idSolicitud: "",
+  });
+
+  const [PrimeroEn, setPrimeroEn] = useState([]);
+  const [catalogoStatus, setcatalogoStatus] = useState([]);
+  const [usuarioxRoles, setusuarioxRoles] = useState([]);
+  const [iniciaApp, setIniciaApp] = useState(true);
+
+  useEffect(() => {
+    const obtenerLineaNegocio = async () => {
+      const response = await catalogosComunes("PrimeroEn");
+      const response2 = await catalogoEstatus();
+      const response3 = await obtenerUsuariosXRoles("Visitador");
+      setPrimeroEn(response.data);
+      setcatalogoStatus(response2.data);
+      setusuarioxRoles(response3.data);
+      //console.log("La Data COmbo", response.data);
+      setIniciaApp(false);
+    };
+
+    obtenerLineaNegocio();
+  }, [iniciaApp]);
+
+  const enviarDatos = () => {
+    setinfoCita({
+      ...infoCita,
+      idSolicitud: idSolicitud,
+    });
+    console.log("La Data Enviar", JSON.stringify(infoCita, null, 2));
+  };
+
+  const idPrimeroEn=(ocupacion)=>{
+    //get id of ocupacion
+    const idOcupacion=PrimeroEn.filter((item)=>item.descripcion===ocupacion)
+    //console.log("idOcupacion",idOcupacion);
+    setinfoCita({
+      ...infoCita,
+      idPrimeroEn: idOcupacion[0].id,
+    });
+  }
+
+  const idVisitador=(visitador)=>{
+    const idVisitador=usuarioxRoles.filter((item)=>item.nombreCompleto===visitador)
+   // console.log("idVisitador",idVisitador);
+    setinfoCita({
+      ...infoCita,
+      idVisitador: idVisitador[0].idUsuario,
+    });
+  }
+
+  const idEstatus=(estatus)=>{
+    const idEstatus=catalogoStatus.filter((item)=>item.descripcion===estatus)
+    //console.log("idEstatus",idEstatus);
+    setinfoCita({
+      ...infoCita,
+      idEstatus: idEstatus[0].estatusId
+    });
+  }
+
   return (
     <Modal
       open={openCita}
-      onClose={handleOpenCita}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
       style={{ overflow: "scroll" }}
+
+
     >
       <Box>
-        <div className="flex flex-col justify- items-center  bg-slate-300 md:w-1/2 w-11/12 mx-auto snap-y">
-          <form className="w-full max-w-lg">
-            <div className="flex flex-col items-center my-10 bg-slate-300 w-full mx-auto">
-              <div className="flex md:flex-row flex-col">
+        <div className="flex flex-col justify- items-center md:w-auto w-11/12 mx-auto snap-y ">
+          <div className="w-full max-w-lg">
+            <div className="flex flex-col items-center my-10 bg-gray-100 w-full mx-auto p-1 rounded-md  ">
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <div className="flex  flex-col">
+                  <div className="flex md:flex-row flex-col justify-between ">
+                    <KeyboardDatePicker
+                      className="md:w-5/12 w-full"
+                      disableToolbar
+                      variant="inline"
+                      format="MM/dd/yyyy"
+                      margin="normal"
+                      id="date-picker-inline"
+                      label="Fecha de inicio"
+                      value={infoCita.fechaInicio}
+                      onChange={(e) => {
+                        setinfoCita({ ...infoCita, fechaInicio: e });
+                      }}
+                      KeyboardButtonProps={{
+                        "aria-label": "change date",
+                      }}
+                    />
+
+                    <KeyboardDatePicker
+                      className="md:w-5/12 w-full px-2"
+                      disableToolbar
+                      variant="inline"
+                      format="MM/dd/yyyy"
+                      margin="normal"
+                      id="date-picker-inline"
+                      label="Fecha de fin"
+                      value={infoCita.fechaFin}
+                      onChange={(e) =>
+                        setinfoCita({ ...infoCita, fechaFin: e })
+                      }
+                      KeyboardButtonProps={{
+                        "aria-label": "change date",
+                      }}
+                    />
+                  </div>
+
+                  <div className="flex md:flex-row flex-col justify-between">
+                    <KeyboardTimePicker
+                      className="md:w-5/12 w-full mx-2"
+                      margin="normal"
+                      id="time-picker"
+                      label="Hora"
+                      value={infoCita.horaInicio}
+                      onChange={(e) =>
+                        setinfoCita({ ...infoCita, horaInicio: e })
+                      }
+                      KeyboardButtonProps={{
+                        "aria-label": "change time",
+                      }}
+                    />
+
+                    <Autocomplete
+                      className="mt-2 md:w-5/12 w-full"
+                      id="combo-box-demo"
+                      options={PrimeroEn}
+                      onChange={(e) =>
+                        idPrimeroEn(e.target.textContent)
+                      }
+                      getOptionLabel={(option) => option.descripcion}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          label="Primero en"
+                          variant="outlined"
+                        />
+                      )}
+                    />
+
+                  </div>
+                  <div className="flex md:flex-row flex-col justify-between">
+                  
+                  <Autocomplete
+                      className="mt-2 md:w-5/12 w-full"
+                      id="combo-box-demo"
+                      options={usuarioxRoles}
+                      getOptionLabel={(option) => option.nombreCompleto}
+                      onChange={(e) =>
+                        idVisitador(e.target.textContent)
+                      }
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          label="Visitador"
+                          variant="outlined"
+                        />
+                      )}
+                    />
+                    <Autocomplete
+                      className="mt-2 md:w-5/12 w-full"
+                      id="combo-box-demo"
+                      options={catalogoStatus}
+                      getOptionLabel={(option) => option.descripcion}
+                      onChange={(e) =>
+                        idEstatus(e.target.textContent)
+                      }
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          label="Estatus"
+                          variant="outlined"
+                        />
+                      )}
+                    />
+                  </div>
+                </div>
+              </MuiPickersUtilsProvider>
+
+              <div className="mt-5  flex mx-auto justify-center w-full  md:px-3 px-16 ">
                 <TextField
-                  id="outlined-multiline-static"
-                  label="Fecha de inicio"
-                  variant="outlined"
-                />
-
-                <div className="flex flex-col justify-center items-center my-3 bg-slate-300 w-5 mx-auto"></div>
-                <TextField
-                  id="outlined-multiline-static"
-                  label="Fecha de fin"
-                  variant="outlined"
-                />
-                <div className="flex flex-col justify-center items-center my-3 bg-slate-300 w-5 mx-auto"></div>
-                <TextField
-                  id="outlined-multiline-static"
-                  label="Hora "
-                  variant="outlined"
-                />
-              </div>
-
-        
-              <div className="mt-5 w-full flex">
-              <Autocomplete
-              className="md:w-full w-7/12 mx-auto"
-                  id="combo-box-demo"
-                  getOptionLabel={(option) => option.title}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Combo box" variant="outlined" />
-                  )}
-                />
-              </div>
-              
-              <div className="mt-5 w-full flex">
-              <Autocomplete
-              className="md:w-full w-7/12 mx-auto"
-                  id="combo-box-demo"
-                  getOptionLabel={(option) => option.title}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Combo box" variant="outlined" />
-                  )}
-                />
-              </div>
-
-              <div className="mt-5 w-full flex">
-              <Autocomplete
-              className="md:w-full w-7/12 mx-auto"
-                  id="combo-box-demo"
-                  getOptionLabel={(option) => option.title}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Combo box" variant="outlined" />
-                  )}
-                />
-              </div>
-
-              <div className="mt-5 w-full flex mx-auto justify-center">
-                <TextField
-              className="md:w-full w-7/12 mx-auto"
-
+                  className="flex w-full  mx-auto"
                   id="outlined-multiline-static"
                   label="Observaciones"
                   variant="outlined"
                   multiline
-                  rows={4}
-                 
+                  minRows={4}
+                  onChange={(e) =>
+                    setinfoCita({ ...infoCita, observaciones: e.target.value })
+                  }
                 />
-
               </div>
-             
-
-
-        
-            
 
               <div className="flex flex-row mt-5">
                 <button
-                  onClick={() => setopenCita(false)}
+                  onClick={() => {
+                    enviarDatos();
+                  }}
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5 md:mt-0 mt-5 "
                 >
                   Guardar
@@ -105,7 +244,7 @@ const ModalagregarCita = ({ openCita, handleOpenCita, setopenCita }) => {
                 </button>
               </div>
             </div>
-          </form>
+          </div>
         </div>
       </Box>
     </Modal>
